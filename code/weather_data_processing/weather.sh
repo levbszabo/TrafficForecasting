db68861e8858b528b3de4272333d22a59b58ecfb
Yaowei Zong <luke0zong@gmail.com>
2020-12-21 00:13:41 -0500

    Update weather.sh

--- code/weather_data_processing/weather.sh
+++ code/weather_data_processing/weather.sh
@@ -2,20 +2,19 @@ CODE="Weather"
 JAR="weather_NYC"
 
 # Erase the hdfs directory
-# hdfs dfs -rm -r -f weather
-
-hdfs dfs -rm -r -f project/output-weather
+hdfs dfs -rm -r -f weather
 
 # Setup the HDFS directory stucture and populate it. 
 
 # Create new directory structure in HDFS
-# hdfs dfs -mkdir weather
+hdfs dfs -mkdir weather
+hdfs dfs -mkdir weather/output-weather
 
 # Populate the input directory in HDFS with the input file
-# hdfs dfs -put input.txt weather
+hdfs dfs -put nyc_weather_2018-2020.3.csv weather
 
 # Verify what is in the input data directory
-hdfs dfs -ls project
+hdfs dfs -ls weather
 
 
 # Build the jar file and run
@@ -33,8 +32,8 @@ javac -classpath `yarn classpath`:. -d . ${CODE}.java
 jar -cvf ${JAR}.jar *.class
 
 # Run the program
-hadoop jar ${JAR}.jar ${CODE} /user/"$USER"/project/nyc_weather_2018-2019.3.csv /user/"$USER"/project/output-weather
+hadoop jar ${JAR}.jar ${CODE} /user/"$USER"/weather/nyc_weather_2018-2020.3.csv /user/"$USER"/weather/output-weather
 
 
 # Output the result (assuming 1 reducer)
-hdfs dfs -cat project/output-weather/part-r-00000
+hdfs dfs -cat weather/output-weather/part-r-00000
